496bb621600cabaedc2d542b1f227ccd907261e4
rajivprahem <70016691+rajivprahem@users.noreply.github.com>
2021-02-10 21:34:57 +0530

    updatejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello welcome to pickkup Hyderbad!\n'
+  const msg = 'Hello node !@#$%^\n'
   res.end(msg);
 });
 
