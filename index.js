50d04de1a42af76771add6fcd7b47030347ada0e
rajivprahem <70016691+rajivprahem@users.noreply.github.com>
2021-02-10 23:17:54 +0530

    committed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello node !@#$%^\n'
+  const msg = 'Hello world welcome adibatla\n'
   res.end(msg);
 });
 
