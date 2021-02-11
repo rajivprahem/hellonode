39304c269ffeea5135c580e1d260859272d0e3ee
rajivprahem <70016691+rajivprahem@users.noreply.github.com>
2021-02-11 19:00:57 +0530

    common

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Please deploy the code!\n'
+  const msg = 'Please deploy the co!\n'
   res.end(msg);
 });
 
