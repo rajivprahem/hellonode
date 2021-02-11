78291bafd22a5a37a6e762aa547fd8a07ace403e
rajivprahem <70016691+rajivprahem@users.noreply.github.com>
2021-02-11 19:09:29 +0530

    peshcodeforever

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Please deploy the co!\n'
+  const msg = 'lease deployment forever\n'
   res.end(msg);
 });
 
